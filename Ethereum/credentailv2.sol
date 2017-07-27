51f3586f8c442f91d66fcd968fb6c57b2983c786
shawn.liu <shawn.liu@cmcm.com>
2017-07-26 17:43:46 -0700

    fix bugs and update

--- Ethereum/credentailv2.sol
+++ Ethereum/credentailv2.sol
@@ -1,21 +1,23 @@
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.13;
 
 contract owned {
   function owned() { owner = msg.sender; }
   address owner;
-  modifier onlyowner { if (msg.sender == owner) _ }
+  modifier onlyowner { if (msg.sender == owner) 
+    _ ;
+  }
 }
 
 contract Credential is owned {
     struct Info {
-        bytes32 public issuer;
-        bytes32 public recipient;
-        bytes public imageHash;
-        bytes public credentialProofDoc;
+        bytes32   issuer;
+        bytes32   recipient;
+        bytes32   imageHash;
+        bytes32   credentialProofDoc;
     }
     Info info;
     uint256 public credentialDate;
-    enum public credentialStatus { Verified, Pending, Final } ;
+    enum credentialStatus { Created, Locked, Inactive }
     credentialStatus status;
 
     function createCredential(bytes32 issuerEntry, bytes32 recipientEntry, uint256 credentialDateEntry, bytes32 credentialEntry, bytes32 descriptionEntry) onlyowner {
@@ -23,31 +25,37 @@ contract Credential is owned {
         info.issuer = issuerEntry;
         info.recipient = recipientEntry;
         credentialDate = credentialDateEntry;
-        status = credentialStatus.Pending
+        status = credentialStatus.Created;
     }
 
     function clear() onlyowner {
         delete info;
     }
 
-    function setStatus(bytes32 statusEntry) onlyowner
+    function setStatusLocked() onlyowner
     {
-        credentialStatus = statusEntry;
-        majorEventFunc(block.timestamp, "Changed Status", statusEntry);
+        status = credentialStatus.Locked;
+        majorEventFunc(block.timestamp, "Changed Status to Locked", "");
+    }
+    
+    function setStatusInactive() onlyowner
+    {
+        status = credentialStatus.Inactive;
+        majorEventFunc(block.timestamp, "Changed Status to Inactive", "");
     }
     
     // Set the IPFS hash of the image of the recipient
-    function setImage(bytes IPFSImageHash) onlyowner
+    function setImage(bytes32 IPFSImageHash) onlyowner
     {
         info.imageHash = IPFSImageHash;
         majorEventFunc(block.timestamp, "Entered Recipient Image", "Image is in IPFS");
     }
     
     // Upload documentation for proof of credential - e.g. graduation certificate
-    function credentialProof(bytes IPFSProofHash) onlyowner
+    function credentialProof(bytes32 IPFSProofHash) onlyowner
     {
         info.credentialProofDoc = IPFSProofHash;
-        majorEventFunc(block.timestamp, "Entered Credential Proof", “Credential proof in IPFS");
+        majorEventFunc(block.timestamp, "Entered Credential Proof", "Credential proof in IPFS");
     }
 
     // Log major events
@@ -68,14 +76,14 @@ contract MultipleCredential is Credential {
     
     function createMultipleCredential(bytes32[] issuerEntries, bytes32[] recipientEntries, uint256 credentialDateEntry, bytes32 descriptionEntry, bytes32[] IPFSImageHashes, bytes32[] IPFSProofHashes) onlyowner {
 
-        assert (issuerEntries.length == recipientEntries.length && issuerEntries.length == IPFSImageHashes.length && issuerEntries.length == IPFSProofHashes.length)
+        assert (issuerEntries.length == recipientEntries.length && issuerEntries.length == IPFSImageHashes.length && issuerEntries.length == IPFSProofHashes.length);
 
         credentialDate = credentialDateEntry;
-        status = credentialStatus.Pending;
+        status = credentialStatus.Created;
 
         for (uint index = 0; index < issuerEntries.length; index++) {
             Infos[IPFSProofHashes[index]].issuer = issuerEntries[index];
-            Infos[IPFSProofHashes[index]].recipientEntry = recipientEntries[index];
+            Infos[IPFSProofHashes[index]].recipient = recipientEntries[index];
             Infos[IPFSProofHashes[index]].imageHash = IPFSImageHashes[index];
             Infos[IPFSProofHashes[index]].credentialProofDoc = IPFSProofHashes[index];
         }
